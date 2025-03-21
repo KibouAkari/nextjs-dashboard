4d61b3b3ab9bc4560813103342176c5692241289
KibouAkari <simeon.schaer1@gmail.com>
2025-03-21 08:51:19 +0100

    Troubleshooting

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,18 +1,16 @@
-import { Suspense } from "react";
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 
-export default async function Page(props: {
-  searchParams?: Promise<{
-    query?: string;
-    page?: string;
-  }>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
 }) {
-  const searchParams = await props.searchParams;
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
 
@@ -22,9 +20,7 @@ export default async function Page(props: {
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Suspense fallback={<div>Loading search...</div>}>
-          <Search placeholder="Search invoices..." />
-        </Suspense>
+        <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -17,6 +17,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete("query");
     }
+
     replace(`${pathname}?${params.toString()}`);
   }
 
